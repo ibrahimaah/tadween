d81fec41f6e4fede8668c105b890521ba5a7c833
ibrahimaah <ibrahim.a.a.h.2017@gmail.com>
2025-05-10 16:01:31 +0300

    create alert-error component

--- resources/views/posts/post_details.blade.php
+++ resources/views/posts/post_details.blade.php
@@ -1,55 +1,34 @@
 @extends('layouts.main_app')
 
-@section('pageTitle')
-{{ __('home.home') }}
-@endsection
+@section('pageTitle', __('home.home'))
+
 
 @section('content')
-
-<div class="post-container">
-    <!-- Bootstrap Delete Confirmation Post Modal -->
-    @include('posts.delete_post_modal',['slugId'=>$post['slug_id']])
+<div class="post-container"> 
     
     <x-page-header title="{{ __('home.home') }}" route="home" />
 
-
-    
-
     <div class="mt-2" id="display-posts-container">
 
-        @if (session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
-        @endif
+        <x-alert-error />
 
-        @if (!empty($post))
+        @isset($post)
             <x-post-component :$post/>
         @else
             <p>🙏</p>
-        @endif
+        @endisset
     </div>
 
     <!-- Create Reply On Posts -->
     {{-- @auth --}}
     @include('partials.reply._reply-form',['post' => $post])
     {{-- @endauth --}}
-
-    
-
 </div>
-
 @endsection
 
-@section('java_scripts')
-    <script src="{{asset('js/posts/display_replies_post.js?version=1.0')}}"></script>
-    {{-- <script src="{{asset('js/posts/post_like.js')}}"></script> --}}
-    @auth
-        {{-- <script src="{{asset('js/posts/delete_post.js?version=1.0')}}"></script> --}}
-        {{-- <script src="{{asset('js/posts/create_reply_post.js?version=1.0')}}"></script> --}}
-        {{-- <script src="{{asset('js/posts/create_post.js?version=1.0')}}"></script> --}}
+@push('js')
+    <script src="{{asset('js/posts/display_replies_post.js?version=1.0')}}"></script> 
+    {{-- @auth  --}}
         <script src="{{asset('js/posts/create_vote_poll.js?version=1.0')}}"></script>
-    @endauth
-
-    
-@endsection
+    {{-- @endauth --}}
+@endpush
